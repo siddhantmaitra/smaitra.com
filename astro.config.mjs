a40281629f59e95d514544c4e4135cd35265ff20
Siddhant Maitra <smaitra.dev@gmail.com>
2022-11-03 02:57:04 +0530

    edit configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,21 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://smaitra.com',
 	integrations: [mdx(), sitemap()],
+
+	markdown: {
+		shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'one-dark-pro',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+	}
+	
 });
