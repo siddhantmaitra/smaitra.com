d2f7c9bab28b7601a203b92e8c0325613f86649d
Siddhant Maitra <smaitra.dev@gmail.com>
2024-04-08 03:25:08 +0530

    misc cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://smaitra.com',
-	integrations: [mdx(), sitemap()],
-
-	markdown: {
-		shikiConfig: {
+  site: 'https://smaitra.com',
+  integrations: [mdx(), sitemap()],
+  output: 'hybrid',
+  markdown: {
+    shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'one-dark-pro',
@@ -18,8 +20,8 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-	}
-	
+      wrap: true
+    }
+  },
+  adapter: process.env.VERCEL === '1' ? vercel() : cloudflare()
 });
