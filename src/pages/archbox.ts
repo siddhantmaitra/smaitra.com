b19d591e9c796f2d445ad81f5750c594c9a0e9d9
Siddhant Maitra <smaitra.dev@gmail.com>
2025-08-11 03:31:20 +0530

    feat: wget support

--- src/pages/archbox.ts
+++ src/pages/archbox.ts
@@ -4,8 +4,10 @@ export const prerender = false;
 export const GET: APIRoute = async ({ params, request }) => {
 
     const response = await fetch("https://raw.githubusercontent.com/siddhantmaitra/scripts/main/working/setup.sh");
-    if (request.headers.get("user-agent")?.startsWith("curl")) {
-        return new Response(await response.text());
+    if (request.headers.get("user-agent")?.startsWith("curl") ||
+        request.headers.get("user-agent")?.startsWith("Wget")
+        ) {
+            return response;
     } else {
         const postURL = new URL(request.url).origin + '/posts/automate-laptop-setup'
         return Response.redirect(postURL);
