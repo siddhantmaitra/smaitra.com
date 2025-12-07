6f720fdbdf45f7f599dd3879ce78eada529bcc56
Siddhant Maitra <smaitra.dev@gmail.com>
2025-12-07 23:19:06 +0530

    chore: update deps to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://smaitra.com',
+  site: "https://smaitra.com",
   integrations: [mdx(), sitemap()],
-  output: process.env.VERCEL === '1' ? 'hybrid' : 'static',
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'one-dark-pro',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      theme: "one-dark-pro",
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  adapter: process.env.VERCEL === '1' ? vercel() : undefined
+  adapter: process.env.VERCEL === "1" ? vercel() : undefined,
 });
